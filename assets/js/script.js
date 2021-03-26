4d449c0ecf294012da2d924f33545cb541ed4bfc
Your Name <you@example.com>
2021-03-26 12:26:19 -0400

    drag and drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var tasks = {};
 
+
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
@@ -131,6 +133,63 @@ $("#remove-tasks").on("click", function() {
   }
   saveTasks();
 });
+
+
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    var tempArr= [];
+
+    $(this).children().each(function() {
+      var text= $(this).find("p").text().trim();
+      var date= $(this).find("span").text().trim();
+      console.log(text, date);
+
+      tempArr.push({
+        text: text,
+        date: date
+      });
+
+    });
+
+    var arrName= $(this).attr("id").replace("list-", "");
+    tasks[arrName]= tempArr;
+    saveTasks();
+
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+ 
+});
+
 
 // load tasks for the first time
 loadTasks();
