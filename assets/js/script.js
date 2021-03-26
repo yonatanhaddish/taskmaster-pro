4197a278184e8b20014736edc79d3aced024ee2f
Your Name <you@example.com>
2021-03-26 18:00:03 -0400

    moment js

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  auditTask(taskLi);
+
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -64,13 +66,36 @@ $(".list-group").on("click", "p",function() {
   })
 });
 
+var auditTask= function(taskEl) {
+  var date= $(taskEl).find("span").text().trim();
+  
+  var time= moment(date, "L").set("hour", 17);
+
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  if(moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days"))<=2) {
+    $(taskEl).addClass("list-group-item-warnings");
+  }
+
+
+};
+
 $(".list-group").on("click", "span", function() {
   var date= $(this).text().trim();
   var dateInput= $("<input>").attr("type", "text").addClass("form-control").val(date);
   $(this).replaceWith(dateInput);
+  dateInput.datepicker( {
+    minDate: 1, 
+    onClose: function() {
+      $(this).trigger("change");
+    }
+  });
   dateInput.trigger("focus");
 
-  $(".list-group").on("blur", "input[type='text']", function() {
+  $(".list-group").on("change", "input[type='text']", function() {
     var date= $(this).val().trim();
     var status= $(this).closest(".list-group").attr("id").replace("list-","");
     var index= $(this).closest(".list-group-item").index();
@@ -79,8 +104,9 @@ $(".list-group").on("click", "span", function() {
     saveTasks();
 
     var taskSpan= $("<span>").addClass("badge badge-primary badge-pill").text(date);
-
     $(this).replaceWith(taskSpan);
+
+    auditTask($(taskSpan)).closest(".list-group-item");
 
   })
 
@@ -189,6 +215,11 @@ $("#trash").droppable({
   }
  
 });
+
+$("#modalDueDate").datepicker( {
+  minDate: 1
+});
+
 
 
 // load tasks for the first time
